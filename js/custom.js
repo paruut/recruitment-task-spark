da3453e9a1ec97718f1aa8ae114b527271e03805
paruut <szymon22521@gmail.com>
2023-01-05 03:48:46 +0100

    non-responsive

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/** INSERT CUSTOM JS HERE **/
+export default function() {
+    document
+    .getElementById("triangle-button")
+    .addEventListener("click", () => {
+        window.open(
+          "https://www.youtube.com/embed/lJSnz_J97-Y",
+          "YouTube Video",
+          "height=315,width=560"
+        );
+      });
+  
+}
